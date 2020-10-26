c589013d16ed7fd91c0a2a1fd09ccde3d2340487
jamess <thiagozevedo3@gmail.com>
2020-10-25 23:02:42 -0300

    fix

--- events/clipsChannelControl.js
+++ events/clipsChannelControl.js
@@ -10,13 +10,13 @@ module.exports = class extends Event {
     }
 
     async run(message) {
-        if (!message.channel.id === '768234397568335873') return;
-        if (message.author.id === '770028698291666944') return;
+        if (message.channel.id !== '768234397568335873') return null;
         
         if(!new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(message.content)) {
+            if (message.author.id === '770028698291666944') return;    
             message.delete();   
             const response = await message.reply('**por favor, use este canal apenas para o envio de clipes!**');
-            response.delete({ timeout: 5000});
+            return response.delete({ timeout: 5000});
         }
         return;
     }
