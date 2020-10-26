001bb742569dd6cc0b1e8311ac26e2f5f0d31ad0
jamess <thiagozevedo3@gmail.com>
2020-10-25 21:30:59 -0300

    clipsChannelControl Event ready

--- events/clipsChannelControl.js
+++ events/clipsChannelControl.js
@@ -5,25 +5,18 @@ module.exports = class extends Event {
     constructor(...args) {
         super(...args, {
             name: 'clipsChannelControl',
-            enabled: true,
             event: 'message',
-            emitter: client,
-            once: false
         });
     }
 
-    run(message) {
+    async run(message) {
         if (!message.channel.id === '768234397568335873') return;
-
-        const isURL = async (message) => {
-            const checkURl = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(message);
-            if (checkURl) return true;
-            return false;
-        }
+        if (message.author.id === '770028698291666944') return;
         
-        if(!isURL) {
-            message.delete();
-            return message.reply('**por favor, use este canal apenas para o envio de clipes!**');
+        if(!new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(message.content)) {
+            message.delete();   
+            const response = await message.reply('**por favor, use este canal apenas para o envio de clipes!**');
+            response.delete({ timeout: 5000});
         }
         return;
     }
