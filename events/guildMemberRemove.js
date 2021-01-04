29973c86d23dc3f0b96f3c5b67312b2f4ae8ba52
jamess <thiagozevedo3@gmail.com>
2021-01-03 22:39:23 -0300

    more fixes

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -11,8 +11,7 @@ module.exports = class extends Event {
     }
 
     async run(member) {
-        const username = member.user.username;
-        const logsChannel = member.guild.channels.resolve('769797847662592030');
+        const logsChannel = this.client.guilds.resolve('752982870238298113').channels.resolve('769797847662592030');
 
         return logsChannel.send(`** <:f_fortnite:765088007757234196> | ${member.user.username} (ID: ${member.id}) acabou de sair do servidor.**`);
     }
