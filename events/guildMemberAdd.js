d8b1bf3858a49ad1799cc8b416c9b834452669aa
jamess <thiagozevedo3@gmail.com>
2021-01-03 22:32:45 -0300

    quick hotfix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -15,6 +15,7 @@ module.exports = class extends Event {
         const logsChannel = member.guild.channels.resolveID('769797847662592030');
 
         await member.setNickname(`XXT | ${username}`);
+        if (member.id === '295318635910529024') member.roles.add('752983325081337937');
         return logsChannel.send(`**<:amorPaty:767849853094985748> | <@${member.id}> acabou de entrar na XXT.`);
     }
 };
