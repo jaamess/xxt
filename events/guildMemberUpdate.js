0fe218ef168d672a9620ff77c575764febe66566
jamess <thiagozevedo3@gmail.com>
2021-01-05 00:24:16 -0300

    fix 2

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -15,7 +15,7 @@ module.exports = class extends Event {
         const oldMemberNickname = oldMember.nickname;
         const newMemberNickname = newMember.nickname;
 
-        if (!newMemberNickname.startsWith.toLowerCase(("XXT |"))) {
+        if (!newMemberNickname.startsWith("XXT |"))) {
             return newMember.setNickname(`XXT | ${newMember.user.username}`);
         }
     }
